80001ac02b919aa54b03ea007fc4929ddd764f07
Xiaode Zheng <your.email@example.com>
2025-12-11 10:19:04 +0800

    fix: layout synax err

---
+++ frontend/src/app/(dashboard)/(clients)/flowers/layout.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "react";
+
+export default function FlowersLayout({
+  children,
+  modal,
+}: {
+  children: ReactNode;
+  modal: ReactNode;
+}) {
+  return (
+    <>
+      {children}
+      {modal}
+    </>
+  );
+}
--- frontend/src/app/(dashboard)/(clients)/layout.tsx
+++ frontend/src/app/(dashboard)/(clients)/layout.tsx
@@ -1,16 +1,5 @@
 import { ReactNode } from "react";
 
-export default function ClientsLayout({
-  children,
-  modal,
-}: {
-  children: ReactNode;
-  modal: ReactNode;
-}) {
-  return (
-    <>
-      {children}
-      {modal}
-    </>
-  );
+export default function ClientsLayout({ children }: { children: ReactNode }) {
+  return <>{children}</>;
 }
